f81d71ab99758628c37f594e951dde55bfc3a4a9
Masibulele22 <41905558+Masibulele22@users.noreply.github.com>
2022-10-25 14:23:46 +0200

    Add files via upload

---
+++ simple_stock_prices.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd 
+
+st.write(""" 
+# Simple Stock Price App
+
+Shown are the closing price and volume of Apple.
+
+""")
+
+#Define the ticker symbol
+tickerSymbol = 'AAPL'
+
+#Get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+#Get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+#Open   High    Low Close   Volume  Dividends   Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
+
