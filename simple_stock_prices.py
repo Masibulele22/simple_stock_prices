4461f07316263a9b6713faa5114f276b10aa4ab9
Masibulele22 <sticky.ngondo@gmail.com>
2024-09-09 22:01:06 +0200

    Update simple_stock_prices.py

--- simple_stock_prices.py
+++ simple_stock_prices.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.write(""" 
 # Simple Stock Price App
 
-Shown are the closing price and volume of Apple.
+Shown are the ***closing price*** and ***volume*** of Apple.
 
 """)
 
@@ -19,6 +19,13 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 #Open   High    Low Close   Volume  Dividends   Stock Splits
 
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+
+st.write("""
+## Volume Price
+""")
 st.line_chart(tickerDf.Volume)
 
